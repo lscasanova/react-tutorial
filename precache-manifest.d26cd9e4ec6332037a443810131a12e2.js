9d2a53cc439d17cf7696a2d9f4f6cfccb006d167
Leandro Santos <leandro.santos@bettatecnologia.com.br>
2020-03-22 15:33:59 -0300

    Updates

---
+++ precache-manifest.d26cd9e4ec6332037a443810131a12e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ed348c1bcb4042327f4fa2b7dac3886",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "1289755f2adf937a70d8",
+    "url": "/react-tutorial/static/css/main.db2fbb6c.chunk.css"
+  },
+  {
+    "revision": "919e78dc0251c3661a37",
+    "url": "/react-tutorial/static/js/2.f876bca9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.f876bca9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1289755f2adf937a70d8",
+    "url": "/react-tutorial/static/js/main.1eb64537.chunk.js"
+  },
+  {
+    "revision": "48186d3ac18516ac0690",
+    "url": "/react-tutorial/static/js/runtime-main.f060380d.js"
+  }
+]);
